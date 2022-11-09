c4784d4a95457c4b6f615e96e031c9c43b86298f
Carlos Sala Samper <carlos15695@hotmail.com>
2022-11-09 18:51:38 +0100

    No console rule added

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   extends: ["plugin:@typescript-eslint/recommended", "google", "prettier"],
-  ignorePatterns: ["dist", 'build'],
+  ignorePatterns: ["dist", "build"],
   rules: {
     "no-unused-vars": "off",
     "require-jsdoc": "off",
-    "new-cap": "off"
-  }
-}
+    "new-cap": "off",
+    "no-console": "error",
+  },
+};
