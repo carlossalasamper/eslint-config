e3e56ff3f5efea29f2cc2f95e6675331697934b5
Carlos Sala Samper <carlos15695@hotmail.com>
2023-08-05 09:24:03 +0200

    0.1.3: added eslint-plugin-unused-imports

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# Carlos Sala Samper - ESLint Config
+# @carlossalasamper/eslint-config
 
-Based on eslint-config-google
+- ✅ Based on [eslint-config-google](https://www.npmjs.com/package/eslint-config-google)
+- 💅 Styled with [prettier](https://prettier.io/)
+- 🛡️ Configured for [TypeScript](https://www.typescriptlang.org/) projects
+- 🏃🏾‍♂️ Designed to speed up and strengthen the development
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   extends: ["plugin:@typescript-eslint/recommended", "google", "prettier"],
+  plugins: ["unused-imports"],
   ignorePatterns: ["dist", "build"],
   rules: {
     "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "warn",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      { vars: "all", args: "after-used", argsIgnorePattern: "^_" },
+    ],
     "require-jsdoc": "off",
     "new-cap": "off",
     "no-console": "error",
